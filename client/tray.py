470eae7ca1286d13065b3a6c446a2e62a1ff6ed0
张林 <zhanglin@192.168.10.104>
2026-04-08 00:36:32 +0800

    fix(tray): add Qt import

--- client/tray.py
+++ client/tray.py
@@ -2,7 +2,7 @@
 
 from PyQt5.QtWidgets import QSystemTrayIcon, QMenu, QAction
 from PyQt5.QtGui import QIcon, QPixmap, QPainter, QColor, QBrush, QPen
-from PyQt5.QtCore import pyqtSignal, QObject
+from PyQt5.QtCore import pyqtSignal, QObject, Qt
 import webbrowser
 
 from client.modes import WorkMode
